5869755985e1a1d57098d3fce266d1bf19facbbb
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-08 13:16:17 +0300

    Initial Commit

---
+++ Tab Bar Usage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tab Bar Usage
+//
+//  Created by Ömer Faruk Kılıçaslan on 8.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tab Bar Usage/Tab_Bar_UsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tab_Bar_UsageApp.swift
+//  Tab Bar Usage
+//
+//  Created by Ömer Faruk Kılıçaslan on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tab_Bar_UsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
